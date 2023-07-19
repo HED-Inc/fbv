c306c613980c5c0bed94da40435b7f09e2428eec
Ankit Panchal <ankit.panchal@einfochips.com>
2023-07-19 11:49:31 +0530

    include

--- fb_display.c
+++ fb_display.c
@@ -18,8 +18,6 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include <linux/fb.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -31,6 +29,7 @@
 #include <asm/types.h>
 #include <string.h>
 #include <errno.h>
+#include <linux/fb.h>
 #include "config.h"
 /* Public Use Functions:
  *
